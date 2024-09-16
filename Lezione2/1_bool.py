c02d4d011552bbf932edfae5bcf3abb7b5ddb5a9
Simone Rover <shushen881@gmail.com>
2024-09-16 21:16:58 +0200

    Add lezione 2 files

---
+++ Lezione2/1_bool.py
@@ -0,0 +1,91 @@
+# Una condizione semplice (senza uso di operatori booleani and/or/not) è chiamata predicato o proposizione.
+# Proposizioni possono essere combinate con gli operatori booleani
+
+a = True
+b = False
+c = False
+
+# NOT - Inverte in predicato
+not_a = not a # False
+
+# AND - True se entrambe (tutte) le proposizioni sono True
+a_and_a = a and a # True
+a_and_b = a and b # False
+b_and_b = b and b # False
+
+# OR - True se almeno una proposizione è True
+a_or_a = a or a # True
+a_or_b = a or b # True
+b_or_b = b or b # False
+
+# Priorità degli operatori booleani
+# 1. NOT
+# 2. AND
+# 3. OR
+# Le parentesi possono cambiare l'ordine di priorità degli operatori (proprio come in algebra!)
+# In caso di parentesi, si calcola prima la condizione più interna.
+
+# Operatori composti
+
+# NAND - Negazione di AND
+not_a_and_a = not a_and_a # False
+not_a_and_b = not a_and_b # True
+not_b_and_b = not b_and_b # True
+
+# NOR - Negazione di OR
+not_a_or_a = not a_or_a # False
+not_a_or_b = not a_or_b # False
+not_b_or_b = not b_or_b # True
+
+# XOR - OR esclusivo, True solo se una E UNA SOLA proposizione è True
+a_xor_a = (a and not a) or (not a and a) # False
+a_xor_b = (a and not b) or (not a and b) # True
+b_xor_b = (b and not b) or (not b and b) # False
+
+# XNOR - Negazione di XOR, True se entrambe (tutte) le proposizioni hanno lo stesso valore di verità
+a_xnor_a = (a and a) or (not a and not a) # True
+a_xnor_b = (a and b) or (not a and not b) # False
+b_xnor_b = (b and b) or (not b and not b) # True
+
+# Le espressioni booleane godono di utili proprietà (proprio come gli operatori matematici)
+
+# PROPRIETA' COMMUTATIVA
+a and b == b and a 
+a or b == b or a 
+
+# PROPRIETA' ASSOCIATIVA
+a or b or c == (a or b) or c == a or (b or c)
+a and b and c == (a and b) and c == a and (b and c)
+
+# PROPRIETA' DISTRIBUTIVA
+a and c or b and c == (a or b) and c
+(a or c) and (b or c) == a and b or c
+
+# Teoremi
+
+# TEOREMA DELL'ANNULLAMENTO
+a and False == False
+a or True == True
+
+# TEOREMA DELL'IDENTITA'
+a and True == a
+a or False == a
+
+# TEOREMA DEI COMPLEMENTI
+a and not a == False
+a or not a == True # Un predicato sempre vero è detto TAUTOLOGIA
+not (a and not a) == True # Tautologia, è il contrario del primo predicato
+
+# TEOREMA DELL'IDEMPOTENZA
+a and a = a
+a or a = a
+
+# TEOREMA DELL'ASSORBIMENTO
+a and (a or b) == a
+a or (a and b) == a
+
+# TEOREMA DI DE MORGAN
+# Il teorema più utilizzato
+# Il not viene distribuito e si cambia l'operatore booleano (and diventa or e viceversa)
+not (a and b) == (not a or not b)
+not (a or b) == (not a and not b)
---
+++ Lezione2/2_funzioni.py
@@ -0,0 +1,75 @@
+# Spesso, occorre eseguire una porzione di codice diverse volte in parti diverse del programma.
+# Possiamo definire delle funzioni con la parola chiave "def".
+# Una funzione è una parte di codice separata dal flusso di esecuzione che può essere eseguita a piacere.
+# Ad esempio, abbiamo già visto alcuni esempi di funzioni come "print" e "input".
+
+# def nome_funzione():
+#   corpo della funzione
+
+# Ora creiamo una nostra funzione che saluta.
+def greet():
+    print("Ciao")
+
+# Ora possiamo salutare quante volte vogliamo:
+greet()
+greet()
+greet()
+
+# Possiamo dare ad una funzione dei dati in ingresso che può usare
+def greet(nome):
+    print("Ciao " + nome)
+
+# Ora possiamo salutare persone diverse:
+greet("Nicola")
+greet("Stefano")
+greet("Giulia")
+
+# Abbiamo definito due funzioni con lo stesso nome, cosa è successo alla vecchia funzione "greet()"? Non è banale!
+# greet()
+
+# Possiamo far coesistere le due funzioni usando un valore di default:
+def greet(nome = None):
+    if (nome is None):
+        print("Ciao")
+    else:
+        print("Ciao " + nome)
+
+greet() # possiamo omettere il parametro "nome"
+greet("Simone")
+
+# NOTA: Prima di chiamare una funzione, questa DEVE essere prima definita.
+
+# Una funziona può terminare restituendo un valore (come la funzione "input")
+def somma(a, b):
+    return a + b
+
+# Ora possiamo sommare agilmente:
+risultato = somma(10, 43)
+print(risultato)
+
+# Una funzione definisce uno "scope", quindi tutte le variabili create in una funzione non sono accessibili dall'esterno!
+# Invece una variabile dichiarata fuori dal corpo della funzione può essere acceduta.
+
+variabile_esterna = 666
+def funzione():
+    print(variabile_esterna)
+funzione()
+
+# Ovviamente, una funzione può essere richiamata all'interno di un altra
+def doppio(x):
+    return somma(x, x)
+
+# Una funzione può richiamare se stessa, questo processo si chiama ricorsione
+# In generale, una funzione ricorsiva è divisa in 3 parti:
+# - Base (controllo condizione di terminazione)
+# - Corpo (dove avviene la computazione)
+# - Passo (viene richiamata la funzione)
+
+def conto_alla_rovescia(n):
+    if (n < 0): # Base
+        return
+
+    print(n) # Corpo
+    conto_alla_rovescia(n-1) # Passo
+
+conto_alla_rovescia(10)
