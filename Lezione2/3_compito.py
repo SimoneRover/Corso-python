a2460ccf94943258d594f44ed30757be7b4a8ac2
Simone Rover <shushen881@gmail.com>
2024-09-17 00:25:44 +0200

    Add compito

---
+++ Lezione2/3_compito.py
@@ -0,0 +1,9 @@
+# 1. [FACILE] Creare una funzione "max" che prende in ingresso due numeri e restituisce il maggiore dei due.
+
+# 2. [INTERMEDIO] Creare una funzione "fattoriale" che prende in ingresso un numero intero e restituisce il suo fattoriale
+
+# 3. [DIFFICILE] Come l'esercizio precedente, ma rendere la funzione ricorsiva
+
+# 4. [DIFFICILE] Un termine della serie di Fibonacci è ottenuto dalla somma dei due termini precedenti.
+# Scrivere una funzione "fibonacci" che dato un numero "n" calcoli e stampi i primi "n" termini della serie di Fibonacci.
+# La serie parte dai due termini 1, 1
